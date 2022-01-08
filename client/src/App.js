f1909dc4323a835b913e18433ddd1272bf6f2cce
Ganesh Balaji Radhakrishnan <ganesh.krishnan237@gmail.com>
2022-01-08 11:49:41 +0100

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from "react"
-import axios from 'axios'
+import React from 'react'
 
 function App() {
-
-  const [data, setData] = useState()
-  axios.get('http://127.0.0.1:8000/').then(res=>//setData(res)
-  console.log(res.data))
-
-  //console.log(data)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div>
+      <p>Rice Project</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
